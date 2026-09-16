e33ede6cd840e71d91deda9a2844fca4ffd23e51
Quoc Bao Dinh Le <q.le102100@gmail.com>
2026-09-16 19:09:38 -0400

    step 8

--- server/public/scripts/gifts.js
+++ server/public/scripts/gifts.js
@@ -1,40 +1,52 @@
-const rednderGifts = async () => {
-  const response = await fetch('/gifts')
-  const data = await response.json()
-  mainContent = document.getElementById('main-content')
+const renderGifts = async () => {
+    
+    const response = await fetch('/gifts')
+    const data = await response.json()
 
-  if (data) {
-    data.map(gift => {
-      const card = document.createElement('div')
-      card.classList.add('card')
-      const topContainer = document.createElement('div')
-      topContainer.classList.add = 'top-container'
-      const bottomContainer = document.createElement('div')
-      bottomContainer.classList.add = 'bottom-container'
-      topContainer.style.backgroundImage = `url(${gift.image})`
-      const name = document.createElement('h3')
-      name.textContent = gift.name
-      bottomContainer.appendChild(name)
-      const price = document.createElement('p')
-      price.textContent = 'Price: ' + gift.price
-      bottomContainer.appendChild(price)
-      const audience = document.createElement('p')
-      audience.textContent = 'Great For: ' + gift.audience
-      bottomContainer.appendChild(audience)
-      const link = document.createElement('a')
-      link.textContent = 'Read More >'
-      link.setAttribute('role', 'button')
-      link.href =  `/gifts/${gift.id}`
-      bottomContainer.appendChild(link)
-      card.appendChild(topContainer)
-      card.appendChild(bottomContainer)
-      mainContent.appendChild(card)
-    })
-  } else {
-    const message = document.createElement('h2')
-    message.textContent = 'No Gift available 😞'
-    mainContent.appendChild(message)
-  }
+    const mainContent = document.getElementById('main-content')
+
+    if (data) {
+
+        data.map(gift => {
+            const card = document.createElement('div')
+            card.classList.add('card')
+
+            const topContainer = document.createElement('div')
+            topContainer.classList.add('top-container')
+
+            const bottomContainer = document.createElement('div')
+            bottomContainer.classList.add('bottom-container')
+
+            topContainer.style.backgroundImage = `url(${gift.image})`
+
+            const name = document.createElement('h3')
+            name.textContent = gift.name
+            bottomContainer.appendChild(name)
+
+            const pricePoint = document.createElement('p')
+            pricePoint.textContent = 'Price: ' + gift.pricePoint
+            bottomContainer.appendChild(pricePoint)
+
+            const audience = document.createElement('p')
+            audience.textContent = 'Great For: ' + gift.audience
+            bottomContainer.appendChild(audience)
+
+            const link = document.createElement('a')
+            link.textContent = 'Read More >'
+            link.setAttribute('role', 'button')
+            link.href = `/gifts/${gift.id}`
+            bottomContainer.appendChild(link)
+
+            card.appendChild(topContainer)
+            card.appendChild(bottomContainer) 
+            mainContent.appendChild(card)
+        })
+    }
+    else {
+        const message = document.createElement('h2')
+        message.textContent = 'No Gifts Available 😞'
+        mainContent.appendChild(message)
+    }
 }
 
-rednderGifts()
+renderGifts()
