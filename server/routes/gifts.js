75332540248fe4461bd107dfea129950dbebec15
Quoc Bao Dinh Le <q.le102100@gmail.com>
2026-09-16 17:38:11 -0400

    Revert "feat: add gifts data and GET /gifts endpoint"
    This reverts commit 521192bea5b74b52bedaa0985cc1019b25ba980b.

--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -1,15 +1,17 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import giftData from '../data/gift'
+import giftData from '../data/gifts.js'
 
 const __filename = fileURLToPath(import.meta.url)
-const __dirnam = path.dirname(_filename)
+const __dirname = path.dirname(__filename)
+
 const router = express.Router()
 
-router.get('/:giftId', (req,res) => {
+router.get('/', (req, res) => {
   res.status(200).json(giftData)
 })
+
 router.get('/:giftId', (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, '../public/gift.html'))
 })
